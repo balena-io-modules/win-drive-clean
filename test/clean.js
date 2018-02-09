fa67ec0a97f584bfa3f2f045126b4c13afa00b89
Jonas Hermsmeier <jhermsmeier@gmail.com>
2018-02-09 20:57:27 +0100

    test: Update test to check noop on non-win32 platforms

--- test/clean.js
+++ test/clean.js
@@ -2,7 +2,13 @@ var assert = require('assert')
 var driveClean = require('..')
 
 context('drive', function () {
-  specify('.clean()', function (done) {
-    driveClean('\\\\.\\PhysicalDriveX', done)
+  context('.clean()', function (done) {
+    specify('should be a NOOP on non-win32 platforms', function(done) {
+      if (process.platform !== 'win32') {
+        driveClean('\\\\.\\PhysicalDriveX', done)
+      } else {
+        this.skip()
+      }
+    })
   })
 })
